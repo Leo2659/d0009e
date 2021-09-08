8eb378daa12b2946f7c53461c45ca2b61be2e007
Leo2659 <ledia_01@edu.sollentuna.se>
2021-09-09 01:22:30 +0200

    s

--- Rekursion och iteration/ekvationslösare.py
+++ Rekursion och iteration/ekvationslösare.py
@@ -1,4 +1,12 @@
 import math
 
 def derivative(f,x,h):
-    return (1/(2*h)*(f*(x+h)-f*(x-h))) 
+    return (1/(2 * h)) * (f(x+h) - f(x-h))    
+
+derivative(math.sin, math.pi, 0.0001)
+
+"""def solve(f,x0,h):
+
+ x - f(x)/derivative(f(x))"""
+        
+
