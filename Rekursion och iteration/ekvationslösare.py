49ec11637170639e6a08a7d55efb94542d98a905
Leo2659 <ledia_01@edu.sollentuna.se>
2021-09-09 23:30:19 +0200

    s

--- Rekursion och iteration/ekvationslösare.py
+++ Rekursion och iteration/ekvationslösare.py
@@ -1,6 +1,5 @@
 import math
 
-
 def derivative(f,x,h):
     return (1/(2 * h)) * (f(x+h) - f(x-h))    
 
@@ -8,8 +7,8 @@ def f(x):
     return x**2+1
 
 def solve(f,x0,h):
-    running = True
-    while running:
+    
+    while True:
         x1 = x0 - (f(x0)/derivative(f,x0,h))
         if abs(x0-x1) < h:
             return x1
@@ -17,5 +16,6 @@ def solve(f,x0,h):
             x0 = x1
 
 import d0009e_lab2_solveTest
+
         
 
