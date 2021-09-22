caab5ed71df0533a076dcf3ef60e71c6ffdae383
Leo2659 <ledia_01@edu.sollentuna.se>
2021-09-22 02:34:52 +0200

    x

--- Ordlista/ordlista.py
+++ Ordlista/ordlista.py
@@ -10,9 +10,9 @@ def printMenu():  # Skriver ut menyn
     print("4: Exit program")
 
 
-def menu(insert, lookup, delete):
-    printMenu()
-    option = int(input("Choose alternative:"))
+def menu(insert, lookup, delete): #Tar in de 3 funktionerna som argument från de 3 olika lösningarna
+    printMenu() #Skriver ut menyn
+    option = int(input("Choose alternative:")) #Låter användaren välja mellan de olika valen
     while option != 4:
         if option == 1:
             insert()
@@ -35,7 +35,7 @@ def main_dic():  # Main funktionen för list versionen av programmet
     descriptions = []  # Lista som innehåller alla förklaringar
 
     def insert_dic():  # Anropas om användaren väljer att lägga till ett ord (Val 1)
-        word = input("Insert: ")
+        word = input("Word to insert: ")
         if word in words:  # Kollar om ordet redan finns i listan
             print("Error: Word already exists!")
         else:
@@ -45,7 +45,7 @@ def main_dic():  # Main funktionen för list versionen av programmet
             descriptions.append(desc)
 
     def lookup_dic():  # Anropas om användaren väljer att söka efter ett ord (Val 2)
-        search = input("Lookup: ")
+        search = input("Word to lookup: ")
         if search not in words:  # Kollar om ordet inte finns i listan
             print("Error: Word does not exists!")
         else:
@@ -54,7 +54,7 @@ def main_dic():  # Main funktionen för list versionen av programmet
             print("Description for",search,": ",descriptions[index])
 
     def delete_dic():  # Anropas om användaren väljer att bort ett ord (Val 3)
-        word = input("Delete: ")
+        word = input("Word to delete: ")
         if word not in words:  # Kollar om ordet inte finns i listan
             print("Error: Word does not exists!")
         else:
@@ -74,7 +74,7 @@ def main_dic2():  # Huvudfuktionen för dictionary version av programmet
     dictionary = {}  # Dictionary med våra ord och förklaringar
 
     def insert_dic2():  # Anropas om användaren väljer att lägga till ett ord (Val 1)
-        word = input("Insert: ")
+        word = input("Word to insert: ")
         if word in dictionary.keys():  # Kollar om ordet matchar en av nycklarna i dictionary
             print("Error: Word already exists!")
         else:  # Lägget till ordet och förklaringen i dictionary
@@ -82,14 +82,14 @@ def main_dic2():  # Huvudfuktionen för dictionary version av programmet
             dictionary.update({word: desc})
 
     def lookup_dic2():  # Anropas om användaren väljer att söka efter ett ord (Val 2)
-        search = input("Lookup: ")
+        search = input("Word to lookup: ")
         if search not in dictionary.keys():
             print("Error: Word does not exist!")
         else:
             print("Description for", search, ":", dictionary[search])
 
     def delete_dic2():  # Anropas om användaren väljer att bort ett ord (Val 3)
-        word = input("Delete: ")
+        word = input("Word to delete: ")
         if word not in dictionary.keys():  # Kollar om ordet inte finns i dictionary
             print("Error: Word does not exist!")
         else:  # Tar bort ordet och förklaringen från dictionary
@@ -107,7 +107,7 @@ def main_dic3():  # Huvudfuktionen för tuple versionen av programmet
     tupleList = []
 
     def insert_dic3():  # Anropas om användaren väljer att lägga till ett ord (Val 1)
-        word = input("Insert: ")
+        word = input("Word to insert: ")
         if isInList(word):  # Kollar om ordet redan finns i listan med funktionen isInList()
             print("Error: Word already exists!")
         else:  # Lägger till ordet och förklaringen om ordet inte finns i listan
@@ -115,7 +115,7 @@ def main_dic3():  # Huvudfuktionen för tuple versionen av programmet
             tupleList.append((word, desc))
 
     def lookup_dic3():  # Anropas om användaren väljer att söka efter ett ord (Val 2)
-        search = input("Lookup: ")
+        search = input("Word to lookup: ")
         if not isInList(search):  # Kollar om ordet inte finns i listan med funktionen isInList()
             print("Error: Word does not exist!")
         else:  # Söker igenom listan och om x[0] alltså något av orden matchar det sökta ordet skrivs ordet x[0] och den matchande förklaringen x[1] ut
@@ -124,7 +124,7 @@ def main_dic3():  # Huvudfuktionen för tuple versionen av programmet
                     print("Description for", x[0], ":", x[1])
 
     def delete_dic3():  # Anropas om användaren väljer att bort ett ord (Val 3)
-        word = input("Delete: ")
+        word = input("Word to delete: ")
         if not isInList(word):  # Kollar om ordet inte finns i listan med funktionen isInList()
             print("Error: Word does not exist!")
         else:  # Söker igenom listan och tar bort hela tuplen med ordet och förklaringen i
